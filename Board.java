6e57062c009df8f89cecf8667f0b9763e8e5e31e
Samuel Igor Palmeira Fernandes <49004249+SIPFernandes@users.noreply.github.com>
2019-03-27 22:55:26 +0000

    adding comments

--- Board.java
+++ Board.java
@@ -10,20 +10,10 @@ class Board {
 	public Board(){ 
 		board = new int[dim][dim];
 	}
-	
 	/**
-	 * Board 
+	 * Board constructor
+	 * @param str		string with the initial position of the 8 queens, one per row
 	 **/
-	public Board(int[] m) throws IllegalStateException {
-		if (m.length != dim*dim) throw new
-		IllegalStateException("Invalid arg in Board constructor");
-		board = new int[dim][dim];
-		int si = 0;
-		for(int i=0; i<dim; i++)
-			for(int j=0; j<dim; j++)
-				board[i][j] = m[si++];
-	}
-	
 	public Board(String str) throws IllegalStateException {
 		if (str.length() != dim) throw new
 		IllegalStateException("Invalid arg in Board constructor");
@@ -31,7 +21,10 @@ class Board {
 		for(int i=0; i<dim; i++)
 			board[i][Character.getNumericValue(str.charAt(i))] = 1;
 	}
-	
+	/**
+	 * Prints the board on console
+	 * @return		the board
+	 **/
 	public String toString() {
 		String s  = new String();
 		for(int i=0; i<dim; i++) {
@@ -42,7 +35,10 @@ class Board {
 		}
 		return s;
 	}
-	
+	/**
+	 * Gets the first empty column it finds
+	 * @return		column index
+	 **/
 	public int getEmptyColumn() {
 		int col = -1;
 		for(int j=0; j<dim; j++) {
@@ -60,11 +56,19 @@ class Board {
 		}
 		return col;
 	}
-	
+	/**
+	 * Sets a queen in a specific position
+	 * @param row		the row index
+	 * @param column	the column index
+	 **/
 	public void setQueen(int row, int column) {
 		board[row][column] = 1;
 	}
-	
+	/**
+	 * switches two rows
+	 * @param row1		the row index
+	 * @param row2		the row index
+	 **/
 	public void rowSwitching(int row1, int row2) {
 		int column1 = getQueenColumn(row1);
 		int column2 = getQueenColumn(row2);
@@ -73,7 +77,10 @@ class Board {
 		board[row1][column2] = 1;
 		board[row2][column1] = 1;
 	}
-	
+	/**
+	* Checks if the board as no conflicts
+	* @return		true if there are no conflicts
+	**/
 	public boolean checkBoard() {
 		for(int i=0; i<dim; i++) {
 			for(int j=0; j<dim; j++) {
@@ -83,7 +90,11 @@ class Board {
 		}
 		return true;
 	}
-	
+	/**
+	 * switches two columns
+	 * @param row1		the row index
+	 * @param row2		the row index
+	 **/
 	public void columnSwitching(int col1, int col2) {
 		int row1 = getQueenRow(col1);
 		int row2 = getQueenRow(col2);
@@ -92,7 +103,11 @@ class Board {
 		board[row1][col2] = 1;
 		board[row2][col1] = 1;
 	}
-	
+	/**
+	 * Gets the row of the queen in the given column
+	 * @param column	column index
+	 * @return		row index
+	 **/
 	public int getQueenRow(int column) {
 		int row = -1;
 		for(int i = 0;  i< dim; i++)
@@ -100,7 +115,11 @@ class Board {
 				row = i;
 		return row;
 	}
-		
+	/**
+	 * Gets the column of the queen in the given row
+	 * @param row		row index
+	 * @return		column index
+	 **/	
 	public int getQueenColumn(int row) {
 		int col = -1;
 		for(int j = 0; j < dim; j++)
@@ -108,7 +127,11 @@ class Board {
 				col = j;
 		return col;
 	}
-	
+	/**
+	 * Checks if there is a position in the row to place a queen without being in conflict and if so places the queen
+	 * @param row		row index
+	 * @return		true if queen placed 		
+	 **/
 	public boolean rowSolve(int row) {
 		for(int j = 0; j < dim; j++) {
 			if(checkQueen(row, j)) {
@@ -118,7 +141,11 @@ class Board {
 		}
 		return false;
 	}
-	
+	/**
+	 * Checks if that row is out of conflict
+	 * @param row		row index
+	 * @return		false if the row is in conflict and removes the queen 		
+	 **/
 	public boolean rowIsSolution(int row) {
 			for(int j = 0; j < dim; j++) {
 				if(board[row][j] != 0 && !checkQueen(row, j)) {
@@ -128,25 +155,25 @@ class Board {
 			}
 			return true;
 	}
-	
-	//retorna a linha que está em conflito
+	/**
+	 * Checks if the specified queen is out of conflict
+	 * @param row		row index
+	 * @param column	column index
+	 * @return		true if queen out of conflict 		
+	 **/
 	public boolean checkQueen(int row, int column) {
-		//check row
 		for(int j = 0; j < dim; j++)
 			if(column != j && board[row][j] == 1)
 				return false;
-		//check column
 		for(int i = 0; i < dim; i++)
 			if(row != i && board[i][column] == 1)
 				return false;
-		//check topLeftDiagonal
 		for(int i = row, j = column; i < dim && j < dim; i++, j++)
 			if(row != i && column != j && board[i][j] == 1)
 				return false;
 		for(int i = row, j = column; i >= 0 && j >= 0; i--, j--)
 			if(row != i && column != j && board[i][j] == 1)
 				return false;
-		//check topRightDiagonal
 		for(int i = row, j = column; i < dim && j >= 0; i++, j--)
 			if(row != i && column != j && board[i][j] == 1)
 				return false;
